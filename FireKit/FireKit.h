de3897d0e458b152adec86d41411326ffd3a694c
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2019-02-24 14:33:38 +0900

    first commit

---
+++ FireKit/FireKit.h
@@ -0,0 +1,19 @@
+//
+//  FireKit.h
+//  FireKit
+//
+//  Created by Takuya Yokoyama on 2019/02/24.
+//  Copyright © 2019 chocoyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FireKit.
+FOUNDATION_EXPORT double FireKitVersionNumber;
+
+//! Project version string for FireKit.
+FOUNDATION_EXPORT const unsigned char FireKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireKit/PublicHeader.h>
+
+
